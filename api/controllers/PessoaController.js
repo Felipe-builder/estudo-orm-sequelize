65082b4fdb3ca6eb14c119d2e76ea2236cc07815
Felipe-builder <felipesvascon@gmail.com>
2022-01-19 14:50:07 -0300

    adicionado o method POST, GET, GET por ID

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -5,8 +5,32 @@ class PessoaController {
         try {
             const todasAsPessoas = await database.Pessoas.findAll()
             return res.status(200).json(todasAsPessoas)
-        }catch(erro) {
-            return res.status(500).json(erro.message)
+        }catch(error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async pegaUmaPessoa(req, res) {
+        const { id } = req.params
+        try{
+            const umaPessoa = await database.Pessoas.findOne({
+                where: {
+                    id: Number(id)
+                }
+            })
+            return res.status(200).json(umaPessoa)
+        } catch (error) {
+            return res.status(404).json(error.message)
+        }
+    }
+
+    static async criaPessoa(req, res) {
+        const novaPessoa = req.body
+        try {
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
+            return res.status(201).json(novaPessoaCriada)
+        } catch (error) {
+            return res.status(500).json(error.message)
         }
     }
 }
