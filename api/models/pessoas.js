5bf3b3f9a9f9a9814568066165a24c9ff1e77f7e
Felipe-builder <felipesvascon@gmail.com>
2022-01-25 17:50:58 -0300

    adicionado escopos, rotas de filtro e ajuste de pegar todos as pessoas, e validacao do formato de e-mail

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -16,12 +16,30 @@ module.exports = (sequelize, DataTypes) => {
   Pessoas.init({
     nome: DataTypes.STRING,
     ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'dado do tipo e-mail inválido'
+        }
+      }
+    },
     role: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'Pessoas',
     paranoid: true,
+    defaultScope: {
+      where: {
+        ativo: true
+      }
+    },
+    scopes: {
+      todos: {
+        where: {}
+      }
+    },
   });
   return Pessoas;
 };
