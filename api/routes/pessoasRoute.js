41a327b1faa036939c1fcff8c9ba4ee23f7aafa3
Felipe-builder <felipesvascon@gmail.com>
2022-01-25 10:51:40 -0300

    criacao das rotas de '/:id/restaura' do Pessoas, Turmas, Niveis, Matriculas

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -8,9 +8,11 @@ router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
 router.post('/pessoas', PessoaController.criaPessoa)
 router.put('/pessoas/:id', PessoaController.atualizaPessoa)
 router.delete('/pessoas/:id', PessoaController.apagaPessoa)
+router.post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
 router.get('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.pegaUmaMatricula)
 router.post('/pessoas/:estudanteId/matriculas', PessoaController.criaMatricula)
 router.put('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.atualizaMatricula)
 router.delete('/pessoas/:estudanteId/matriculas/:matriculaId', PessoaController.apagaMatricula)
+router.post('/pessoas/:estudanteId/matriculas/:matriculaId/restaura', PessoaController.restauraMatricula)
 
 module.exports = router
